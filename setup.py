42d17af3a4f1df0da947f3df8abbada7dff1e3e2
Bradley Tse <bradleytse@gmail.com>
2013-12-27 19:35:51 -0500

    Added gitignore. Updated setup script and zshrc PATH env.

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
+###############################################################################
 # Author: Bradley Tse
 # Email: bradleytse@gmail.com
 #
-# A simple script to set up important config files
+# A simple script that creates a bunch of symlinks to the different config
+# files in the configfiles directory. The idea is that you should be able
+# to just clone this git repo, and then run "python setup.py".
 #
 # To add more configs to keep track of, just add the file to the config folder
-# and then add an entry to the configlist file
+# and then add an entry to "configlist.txt". You should then either manually
+# create a symlink to the newly added config file, or run setup.py. Either way,
+# make sure that any changes you make to the config file are to the one in the
+# configfiles directory.
+#
+##############################################################################
 
 import os
 import sys
@@ -14,13 +22,13 @@ import sys
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_FILE = os.path.join(SCRIPT_DIR, "configlist.txt")
 
-# Scans the configlist.txt file and returns a dictionary of the config name
-# and install location
+
 def scan_config_list():
+    """Scans configlist.txt and returns a dictionary of config names to locations"""
     print("parsing configlist.txt")
     config_dict = {}
-    with open(CONFIG_FILE, 'r') as file:
-        for linenum, line in enumerate(file):
+    with open(CONFIG_FILE, 'r') as f:
+        for line_num, line in enumerate(f):
             line = line.strip().rstrip('\n')
 
             # Ignore comments
@@ -30,53 +38,50 @@ def scan_config_list():
             split = line.split(':')
 
             # TODO Add more error checking
-            if len(split) > 2:
-                sys.exit("Incorrect entry at line {0}".format(linenum + 1))
+            if len(split) != 2:
+                sys.exit("Incorrect entry at line {0}".format(line_num + 1))
 
             config_dict[split[0]] = split[1]
 
     return config_dict
 
 
-# Create the symbolic links
-# config: file name in the configfiles folder
-# path: path of symbolic link
 def create_link(config, path):
+    """Create a symlink from config to path"""
     config_path = os.path.join(SCRIPT_DIR, config)
     install_path = os.path.expanduser(path)
     if os.path.exists(install_path):
-        while(True):
+        while True:
             ans = raw_input("overwrite {0}? (y/n) ".format(install_path))
-            if ans == "y" or ans == "Y":
+            if ans in ['y', 'Y', 'yes', 'Yes', 'YES']:
                 print("creating symlink from {0} to {1}".format(
                       install_path, config_path))
                 os.remove(install_path)
                 os.symlink(config_path, install_path)
                 break
-            elif ans == "n" or ans == "N":
+            elif ans in ['n', 'N', 'no', 'No', 'NO']:
                 print("skipping")
                 return
     else:
-        while(True):
+        while True:
             ans = raw_input("create symlink for {0}? (y/n) ".format(
                             install_path))
-            if ans == "y" or ans == "Y":
-                break
-            elif ans == "n" or ans == "N":
+            if ans in ['y', 'Y', 'yes', 'Yes', 'YES']:
+                print("creating symlink from {0} to {1}".format(
+                    install_path, config_path))
+                os.symlink(config_path, install_path)
+                return
+            elif ans in ['n', 'N', 'no', 'No', 'NO']:
                 print("skipping")
                 return
-        print("creating symlink from {0} to {1}".format(
-              install_path, config_path))
-        os.symlink(config_path, install_path)
-        return
+
 
 def main():
-
     config_dict = scan_config_list()
     for config, path in config_dict.iteritems():
         create_link(config, path)
 
-    print("script succesful")
+    print("script successful")
 
 if __name__ == '__main__':
     sys.exit(main())
